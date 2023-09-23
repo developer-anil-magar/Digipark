847af442f162eaa7ae03dd8602b24efb2f8b790a
developer-anil-magar <anilmagar.developer@gmail.com>
2023-09-23 06:21:24 +0545

    replaced png with webp

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,7 +1,7 @@
 import Map from './Maps.webp';
-import Iphone from './iPhone.png';
+import Iphone from './iPhone.webp';
 import Eren from './Eren.png';
 import Mikasa from './Mikasa.png';
-import IphoneMap from './iPhone-2.png';
+import IphoneMap from './iPhone-2.webp';
 
 export { Map, Iphone, Eren, Mikasa, IphoneMap };
