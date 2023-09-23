b4f4805c70e48159d9257d11ae403b3ce89c91cf
developer-anil-magar <anilmagar.developer@gmail.com>
2023-09-23 06:25:14 +0545

    added back png

--- public/assets/images/index.js
+++ public/assets/images/index.js
@@ -1,7 +1,7 @@
 import Map from './Maps.webp';
-import Iphone from './iPhone.webp';
+import Iphone from './iPhone.png';
 import Eren from './Eren.png';
 import Mikasa from './Mikasa.png';
-import IphoneMap from './iPhone-2.webp';
+import IphoneMap from './iPhone-2.png';
 
 export { Map, Iphone, Eren, Mikasa, IphoneMap };
